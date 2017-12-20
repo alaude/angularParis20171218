f6eb370b7330602c04e43844d9b93b37368b8381
alaude <aznive.laude@gmail.com>
2017-12-20 15:38:32 +0100

    add routing

--- my-app/src/app/page-not-found/page-not-found.module.ts
+++ my-app/src/app/page-not-found/page-not-found.module.ts
@@ -5,9 +5,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { PageNotFoundRoutingModule } from './page-not-found-routing.module';
 
-const appRoutes: Routes = [
-  { path: '**', component: PageNotFoundComponent }
-];
 
 @NgModule({
   imports: [
